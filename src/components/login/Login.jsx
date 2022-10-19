41153cc76e579da58231e60ffe4b0d6a413f4a4d
Tiberius Mairura <tiberiusmairura.dev@gmail.com>
2022-10-19 20:44:06 +0300

    install mui, setup client side routing

---
+++ src/components/login/Login.jsx
@@ -0,0 +1,12 @@
+import Button from "@mui/material/Button";
+import React from "react";
+
+function Login() {
+  return (
+    <div>
+      <Button variant="outlined">Login</Button>
+    </div>
+  );
+}
+
+export default Login;
---
+++ src/components/signup/Singnup.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Singnup() {
+  return <div>Singnup</div>;
+}
+
+export default Singnup;

2d2c07ef5e507b8704fced928219ce505aca63c1
denis maina <denis.maina@student.moringaschool.com>
2022-10-20 17:44:30 +0300

    created form

--- src/components/signup/Singnup.jsx
+++ src/components/signup/Singnup.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 function Singnup() {
-  return <div>Singnup</div>;
+  return(
+    <div className="container">
+      <div className="contact-box">
+        <div className="left"></div>
+        <div className="right">
+          <form>
+          <h1>Sign up here</h1>
+          <input type="text" className="field" placeholder="firstName"/>
+          <input type="text" className="field" placeholder="lastName"/>
+          <input type="text" className="field" placeholder="email"/>
+          <input type="text" className="field" placeholder="phone-number"/>
+          <input type="text" className="field" placeholder="password"/>
+          <input type="text" className="field" placeholder="password confirmation"/>
+          <button className="btn">REGISTER</button>
+
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Singnup;

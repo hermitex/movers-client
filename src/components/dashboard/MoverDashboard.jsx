a36fb2904c42dd87269d9c75509004a086c676ef
denis maina <denis.maina@student.moringaschool.com>
2022-10-24 16:59:05 +0300

    dashboard sidebar created

--- src/components/dashboard/MoverDashboard.jsx
+++ src/components/dashboard/MoverDashboard.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import MoverSideBar from "./MoverSideBar";
 
 function MoverDashboard(){
     return(
         <div className="profile-container"> 
            <div className="profile-item-1">
-                <p>Hi  am the one contained here</p>
+            <MoverSideBar/>
             </div> 
            <div className="profile-item-2">
                 <p>This is Division 2</p>
---
+++ src/components/dashboard/MoverSideBar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function MoverSideBar(){
+    return(
+        <div>
+            <div>
+                <h1>MOOOVE.com</h1>
+                <div>
+                <div>profile photo here</div>
+                <h2>Tiberius Mairura</h2>
+                <h6>My Profile</h6>
+                </div>
+                <div>
+                    <ul>
+                        <li>Dashboard</li>
+                        <li>Completed Orders</li>
+                        <li>Financial Analytics</li>
+                        <li>Reviews</li>
+                        <li>Location Anlalytics</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MoverSideBar;

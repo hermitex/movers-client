95f6b30c330c08e799dedf173a1c5885ef4b841b
denis maina <denis.maina@student.moringaschool.com>
2022-10-25 10:20:26 +0300

    updated dashboard home

---
+++ src/components/dashboard/DashboardHome.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function DashboardHome(){
+    return(
+        <div>Here are where the mover details are seen.</div>
+    )
+}
+
+export default DashboardHome;
--- src/components/dashboard/MoverDashboard.jsx
+++ src/components/dashboard/MoverDashboard.jsx
@@ -1,15 +1,28 @@
 import React from "react";
 import MoverSideBar from "./MoverSideBar";
+import Orders from './Orders'
+import { Route, Routes } from "react-router-dom";
+import DashboardHome from "./DashboardHome";
+
 
 function MoverDashboard(){
     return(
         <div className="profile-container"> 
-           <div className="profile-item-1">
-            <MoverSideBar/>
-            </div> 
-           <div className="profile-item-2">
-                <p>This is Division 2</p>
-            </div> 
+
+            <div className="profile-item-1">
+                <MoverSideBar/>
+                </div> 
+            <div className="profile-item-2">
+                <Routes>
+                <Route
+                    path="/dashboard/home"
+                    element={<DashboardHome/>}/>
+                    <Route
+                    path="/dashboard/orders"
+                    element={<Orders/>}/>
+                </Routes>
+            </div>
+ 
         </div>
     )
 }

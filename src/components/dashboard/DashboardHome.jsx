54dc2bfce132131ce44e9f9ae5f366a7cb1f4d39
denis maina <denis.maina@student.moringaschool.com>
2022-10-25 16:42:17 +0300

    updated data in home component

--- src/components/dashboard/DashboardHome.jsx
+++ src/components/dashboard/DashboardHome.jsx
@@ -10,12 +10,12 @@ function DashboardHome(){
             </div>
             <div className="mover-1">
                 <h4>EARNINGS</h4>
-                <h6>98</h6>
+                <h6>Ksh. 250,000</h6>
                 <p>See details</p>
             </div>
             <div className="mover-1">
                 <h4>BALANCE</h4>
-                <h6>98</h6>
+                <h6>Ksh. 200,000</h6>
                 <p>See details</p>
             </div>
         </div>

a53e4f2e37f37e0517de5748049452ee1a1e2b44
denis maina <denis.maina@student.moringaschool.com>
2022-10-27 10:51:18 +0300

    initial changes to book

--- src/components/moving-process/Book.jsx
+++ src/components/moving-process/Book.jsx
@@ -1,13 +1,15 @@
-import { Paper, Typography } from '@mui/material'
-import { Box, Container } from '@mui/system'
-import React from 'react'
+import { Paper, Typography } from "@mui/material";
+import TextField from "@mui/material/TextField";
+import { Box, Container } from "@mui/system";
+import React from "react";
 
-import MovingProcessNavBar from './MovingProcessNavBar';
+import MovingProcessNavBar from "./MovingProcessNavBar";
 
 function Book() {
   const styles = {
     paperContainer: {
-      backgroundImage: "linear-gradient(to bottom, rgba(0, 0, 0, 0.52), rgba(0, 0, 0, 0.73)), url('https://images.pexels.com/photos/4246196/pexels-photo-4246196.jpeg?auto=compress&cs=tinysrgb&w=1600')",
+      backgroundImage:
+        "linear-gradient(to bottom, rgba(0, 0, 0, 0.52), rgba(0, 0, 0, 0.73)), url('https://images.pexels.com/photos/4246196/pexels-photo-4246196.jpeg?auto=compress&cs=tinysrgb&w=1600')",
       height: "auto",
       backgroundPosition: "center",
       backgroundRepeat: "no-repeat",
@@ -15,15 +17,13 @@ function Book() {
       borderRadius: 0,
     },
     paperStyle: {
-      backgroundColor: "#F2F2F2", 
-      padding :20, 
-      height: 800, 
-      width: 800, 
-      margin: '100px auto'
-    }
+      backgroundColor: "#F2F2F2",
+      padding: 20,
+      height: 800,
+      width: 800,
+      margin: "100px auto",
+    },
   };
-  
-
 
   return (
     <Box>
@@ -51,16 +51,37 @@ function Book() {
               </Typography>
             </Box>
             <Box>
-              <Paper elevation={7} style={styles.paperStyle} >
+              <Paper elevation={7} style={styles.paperStyle}>
                 <MovingProcessNavBar size="large" color="error" />
-                
+                <Box>
+                  <div>
+                    <h1>Book your Mooove! today ...</h1>
+                    <p>Secured payment - No extra charges</p>
+                  </div>
+                </Box>
+                <Box>
+                  <Box
+                    component="form"
+                    sx={{
+                      "& > :not(style)": { m: 1, width: "25ch" },
+                    }}
+                    noValidate
+                    autoComplete="off"
+                  >
+                    <TextField
+                      id="outlined-basic"
+                      label="Outlined"
+                      variant="outlined"
+                    />
+                  </Box>
+                </Box>
               </Paper>
-            </Box>            
+            </Box>
           </Box>
         </Container>
-      </Paper>      
+      </Paper>
     </Box>
-  )
+  );
 }
 
-export default Book
+export default Book;
